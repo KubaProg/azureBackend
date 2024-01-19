c2b9c423b5a5c22a3a9c9766e7cd0daf74aa195b
Jakub_Opielka <jakub.opielka@q-perior.com>
2024-01-19 23:34:56 +0100

    method improved to handle react request

--- src/main/java/pl/kuba/azureaitextanalythicsaproject/Controller.java
+++ src/main/java/pl/kuba/azureaitextanalythicsaproject/Controller.java
@@ -3,6 +3,9 @@ package pl.kuba.azureaitextanalythicsaproject;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,7 +14,15 @@ public class Controller {
     private final Analyzer analyzerService;
 
     @PostMapping("/summarize")
-    public ResponseEntity<Response> retrieveSummary(@RequestBody Request request){
+    public ResponseEntity<Response> retrieveSummary(
+            @RequestParam("keywords") List<String> keyWords,
+            @RequestParam("length") String length,
+            @RequestParam("file") MultipartFile file) {
+        Request request = Request.builder()
+                .keyWords(keyWords)
+                .length(length)
+                .file(file)
+                .build();
         return ResponseEntity.ok(this.analyzerService.summarizationExample(request));
     }
 
